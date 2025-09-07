fe273e0549915da72b89bd967bfa33aa77e0f51c
Khan-Ramsha <khanramsha302020@gmail.com>
2025-09-07 11:43:00 +0530

    nesterov accelerated GD

---
+++ nesterov_accelerated.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+#these are direct results of partial derivatives. 
+def derivative(predictions, x, y):
+    d_theta0 = np.mean((predictions - y))
+    d_theta1 = np.mean(x * (predictions - y))
+    return d_theta0, d_theta1
+
+def gradient_descent(theta0, theta1, d_theta0, d_theta1, lr):
+    theta0 -= lr * d_theta0
+    theta1 -= lr * d_theta1
+    return theta0, theta1
+
+#exponentially weighted avg of current and past gradients. 
+
+prev_theta0, prev_theta1, beta = 0, 0, 0.9
+def avg_gradients(prev_theta0, prev_theta1 , d_theta0, d_theta1):
+    prev_theta0 = beta * prev_theta0 + lr * d_theta0
+    prev_theta1 = beta * prev_theta1 + lr * d_theta1
+
+    return prev_theta0, prev_theta1 #so far gradients
+
+theta0, theta1 = 0, 0
+x = np.arange(1, 20)
+y = 2 * x + 5
+lr = 0.001
+for i in range(10):
+    look_ahead_theta0 =  theta0 - lr * beta * prev_theta0       #look ahead
+    look_ahead_theta1 =  theta1 - lr * beta * prev_theta1        #look ahead
+    predictions = look_ahead_theta0 + look_ahead_theta1 * x
+    d_theta0, d_theta1 = derivative(predictions, x, y) #calculate the gradient at look ahead point
+    prev_theta0, prev_theta1 = avg_gradients(prev_theta0, prev_theta1, d_theta0, d_theta1)
+    theta0, theta1 = gradient_descent(theta0, theta1, prev_theta0, prev_theta1, lr)
+    print(theta0, theta1)
